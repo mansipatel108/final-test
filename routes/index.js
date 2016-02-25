a30363fe69792b2e8c63e5971911e31f7ec01d3c
Mansi <mpatel.y@gmail.com>
2016-02-25 13:32:02 -0500

    modifying template

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home/landing page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Express' });
+/* GET people page. */
+router.get('/people', function(req, res, next) {
+  res.render('people', { title: 'Express' });
 });
+
+/* GET cotact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
 
 module.exports = router;
